ae3abaacb1132e24e867eb5a3f4b6386eeaa5653
roquefore <roquefore@gmail.com>
2019-08-27 13:43:12 +0300

    Spinner-loader

--- src/components/_shared/Spinner/index.js
+++ src/components/_shared/Spinner/index.js
@@ -4,15 +4,15 @@ import React from 'react';
 // Styles
 import Styles from './styles.module.scss';
 
-const Spinner = ({ className, size, children }) => (
+const Spinner = ({ className, size, children, loader }) => (
     <section
-        className={`${Styles.container} ${className}`}
+        className={`${Styles.container} ${className} `}
         style={{
             height: size,
             width: size,
         }}
     >
-        <div className={Styles.spinner} />
+        <div className={`${Styles.spinner} ${loader && Styles.loader}`} />
         <div className={Styles.children}>{children}</div>
     </section>
 );
